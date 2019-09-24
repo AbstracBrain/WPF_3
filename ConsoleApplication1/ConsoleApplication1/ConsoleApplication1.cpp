b2280d3f9023aca5a9760cbbf4cbdf5ffa93a28c
Alumno <Alumno@PC31>
2019-09-24 09:41:36 -0500

    sponlia

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	cout << "========================================\n";
+	cout << "Bienvenido a este programa que te permite calcular el area de un triangulo. \n";
+	cout << "========================================= \n";
+	float altura, area, base;
+
+	cout << "Introduce la base: ";
+	cin >> base;
+	cout << "Introduce la altura: ";
+	cin >> altura;
+	area = base * altura / 2;
+	cout << "El area del triangulo es: " << area;
+	return 0;
+}
